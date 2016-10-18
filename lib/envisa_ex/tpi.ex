0d1a703d8b63297748be0c9a59a6ad984e24b906
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2016-10-18 12:45:22 -0400

    initial implementation for encoding/decoding TPI commands

---
+++ lib/envisa_ex/tpi.ex
@@ -0,0 +1,45 @@
+defmodule EnvisaEx.TPI do
+  @moduledoc """
+  This module includes the needed functions to encode and decode requests / responses
+  from / to the EnvisaLink TPI hardware module.
+  """
+
+  @doc """
+  Takes a binary string and encodes it into base16 then appends the checksum and the EOLs.
+  """
+  def encode(string) do
+    string <> checksum(string) <> "\r\n"
+    |> Base.encode16
+  end
+
+  @doc """
+  Takes a base16 string and decodes it then validates the checksum.
+  """
+  def decode(encoded_string) do
+    string = Base.decode16!(encoded_string)
+
+    if valid?(string), do: {:ok, string}, else: {:error, string}
+  end
+
+  @doc """
+  Takes a binary string and validates it using the checksum.
+  """
+  def valid?(string) do
+    data_bytes_size = byte_size(string) - 4
+
+    <<command_and_data::binary-size(data_bytes_size), cks::binary-size(2), _eols::binary-size(2)>> = string
+
+    cks == checksum(command_and_data)
+  end
+
+  @doc """
+  Takes a binary string and calculates its checksum.
+  """
+  def checksum(string) do
+    String.codepoints(string)
+    |> Enum.map(fn element -> element |> Base.encode16 end)
+    |> Enum.reduce(0, fn element, acc -> String.to_integer(element, 16) + acc end)
+    |> Integer.to_string(16)
+    |> binary_part(0, 2)
+  end
+end
---
+++ test/envisa_ex/tpi_test.exs
@@ -0,0 +1,28 @@
+defmodule EnvisaEx.TPITest do
+  use ExUnit.Case
+  doctest EnvisaEx.TPI
+
+  test "request encoding is correct" do
+    assert EnvisaEx.TPI.encode("0056Secret") == "3030353653656372657433330D0A"
+  end
+
+  test "valid-response decoding is correct" do
+    assert EnvisaEx.TPI.decode("3030353653656372657433330D0A") == {:ok, "0056Secret33\r\n"}
+  end
+
+  test "invalid-response decoding is correct" do
+    assert EnvisaEx.TPI.decode("3030353653656372657433340D0A") == {:error, "0056Secret34\r\n"}
+  end
+
+  test "correctly identifies valid commands" do
+    assert EnvisaEx.TPI.valid?("0056Secret33\r\n")
+  end
+
+  test "checksum is correct" do
+    assert EnvisaEx.TPI.checksum("6543") == "D2"
+  end
+
+  test "checksum is truncated to 1 byte" do
+    assert EnvisaEx.TPI.checksum("0056Secret") == "33"
+  end
+end
