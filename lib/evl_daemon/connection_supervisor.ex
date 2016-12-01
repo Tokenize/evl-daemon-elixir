46387ffd82d206be6fe39842b77031e8c6f62e58
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2016-12-01 00:24:21 -0500

    Clean-up ConnectionSupervisor by removing start_workers

--- lib/evl_daemon/connection_supervisor.ex
+++ lib/evl_daemon/connection_supervisor.ex
@@ -2,19 +2,16 @@ defmodule EvlDaemon.ConnectionSupervisor do
   use Supervisor
 
   def start_link(opts) do
-    result = {:ok, sup} = Supervisor.start_link(__MODULE__, opts)
-    start_workers(sup, opts)
-    result
+    {:ok, _pid} = Supervisor.start_link(__MODULE__, opts)
   end
 
-  def init(_) do
-    supervise([], strategy: :one_for_one)
-  end
-
-  def start_workers(sup, opts) do
-    {:ok, connection} = Supervisor.start_child(sup, worker(EvlDaemon.Connection, [opts]))
+  def init(opts) do
+    child_processes = [
+      worker(EvlDaemon.Connection, [opts])
+    ]
 
     EvlDaemon.Connection.connect(connection)
     EvlDaemon.Connection.command(connection, "005#{opts.password}")
+    supervise(child_processes, strategy: :one_for_one)
   end
 end
