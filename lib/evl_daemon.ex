6a1a48c0a43d4f40ff7ca2bcad15b7d514d05916
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2016-12-14 12:58:13 -0500

    Log a message to the console on starting up

--- lib/evl_daemon.ex
+++ lib/evl_daemon.ex
@@ -1,7 +1,12 @@
 defmodule EvlDaemon do
+  require Logger
   use Application
 
+  @version Mix.Project.config[:version]
+
   def start(_type, _args) do
+    Logger.info "Starting EvlDaemon v#{@version}"
+
     EvlDaemon.Supervisor.start_link(%{event_dispatcher: nil})
   end
 end
