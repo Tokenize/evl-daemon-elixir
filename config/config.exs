5da91b90aa03bcd69eb56afdce4684b9d0c7bfa0
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2017-01-22 21:33:24 -0500

    Set default event_notifiers to none in config
    They can be configured by editing the evl_daemon.conf file.

--- config/config.exs
+++ config/config.exs
@@ -6,11 +6,7 @@ config :evl_daemon, host: '127.0.0.1'
 config :evl_daemon, port: 4025
 config :evl_daemon, password: "SECRET"
 config :evl_daemon, auto_connect: false
-config :evl_daemon, event_notifiers: [
-  [type: :console],
-  [type: :email, recipient: "user@example.com", sender: "noreply@example.com"],
-  [type: :sms, from: "+1234567890", to: "+10987654321", sid: "SID", auth_token: "SECRET"]
-]
+config :evl_daemon, event_notifiers: []
 config :evl_daemon, zones: %{}
 config :evl_daemon, system_emails_sender: "noreply@example.com"
 config :evl_daemon, system_emails_recipient: "user@example.com"
