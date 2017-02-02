f274d8e1a6858c52b6467a644c24a606c65c1877
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2017-02-02 12:34:29 -0500

    Add module description for ErrorNotifier
    references #3

--- lib/evl_daemon/error_notifier.ex
+++ lib/evl_daemon/error_notifier.ex
@@ -1,8 +1,16 @@
 defmodule EvlDaemon.ErrorNotifier do
+  @moduledoc """
+  This module overrides the terminate/2 method from GenServer and sends
+  an email notification if the termination reason isn't :normal / :shutdown.
+  """
+
   defmacro __using__(_) do
     quote location: :keep do
       require Logger
 
+      # Callbacks
+
+      @doc false
       def terminate(reason, _state) when reason in [:normal, :shutdown], do: nil
       def terminate(reason, _state) do
         EvlDaemon.Email.Application.process_terminating(__MODULE__, reason) |> EvlDaemon.Mailer.deliver_now
