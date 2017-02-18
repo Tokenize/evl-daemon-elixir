369d4a35b14a889c7bfbec0cc72c1b8ee4ea38d3
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2017-02-17 21:14:23 -0500

    Add test for Plug.AuthTokenValidator's response

--- test/evl_daemon/router_test.exs
+++ test/evl_daemon/router_test.exs
@@ -29,6 +29,16 @@ defmodule EvlDaemon.RouterTest do
     assert conn.status == 401
   end
 
+  test "returns a JSON encoded error if auth token is missing" do
+    conn = conn(:get, "/events?auth_token=invalid")
+            |> EvlDaemon.Router.call(@opts)
+
+    decoded_response = Poison.decode!(conn.resp_body)
+
+    assert Map.has_key?(decoded_response, "error") == true
+    assert Map.get(decoded_response, "error") == "You need to specify a valid authentication token."
+  end
+
   test "returns 404 when accessing an invalid endpoint" do
     Application.put_env(:evl_daemon, :auth_token, "secret")
 
