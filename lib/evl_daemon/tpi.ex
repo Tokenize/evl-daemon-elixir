d897f5dfc1baac58f0d58c5cd41bf683829ef4a9
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2019-05-30 18:48:05 -0400

    move @spec statements under @doc in TPI module

--- lib/evl_daemon/tpi.ex
+++ lib/evl_daemon/tpi.ex
@@ -6,28 +6,28 @@ defmodule EvlDaemon.TPI do
 
   use Bitwise
 
-  @spec encode(String.t()) :: String.t()
   @doc """
   Takes a binary string and appends the checksum and the EOLs.
   """
+  @spec encode(String.t()) :: String.t()
   def encode(string) do
     string <> checksum(string) <> "\r\n"
   end
 
-  @spec decode(String.t()) :: {:ok | :error, String.t()}
   @doc """
   Takes a binary string and trims it then validates the checksum.
   """
+  @spec decode(String.t()) :: {:ok | :error, String.t()}
   def decode(encoded_string) do
     string = String.trim(encoded_string)
 
     if valid?(encoded_string), do: {:ok, string}, else: {:error, encoded_string}
   end
 
-  @spec valid?(String.t()) :: boolean
   @doc """
   Takes a binary string and validates it using the checksum.
   """
+  @spec valid?(String.t()) :: boolean
   def valid?(string) do
     data_bytes_size = byte_size(string) - 4
 
@@ -37,10 +37,10 @@ defmodule EvlDaemon.TPI do
     cks == checksum(command_and_data)
   end
 
-  @spec checksum(String.t()) :: String.t()
   @doc """
   Takes a binary string and calculates its checksum.
   """
+  @spec checksum(String.t()) :: String.t()
   def checksum(string) do
     String.codepoints(string)
     |> Enum.map(fn element -> element |> Base.encode16() end)
@@ -50,18 +50,18 @@ defmodule EvlDaemon.TPI do
     |> String.pad_leading(2, ["0"])
   end
 
-  @spec command_part(String.t()) :: String.t()
   @doc """
   Takes a binary string representing a TPI message and returns its command part.
   """
+  @spec command_part(String.t()) :: String.t()
   def command_part(string) do
     String.slice(string, 0..2)
   end
 
-  @spec data_part(String.t()) :: String.t()
   @doc """
   Takes a binary string representing a TPI message and returns its data part.
   """
+  @spec data_part(String.t()) :: String.t()
   def data_part(string) do
     String.slice(string, 3..-3)
   end
