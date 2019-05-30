5b0425df4b628df2291c200e250e22d895641347
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2019-05-30 18:53:53 -0400

    bump EvlDaemon version to 0.3.0

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ If [available in Hex](https://hex.pm/docs/publish), the package can be installed
 
     ```elixir
     def deps do
-      [{:evl_daemon, "~> 0.2.0"}]
+      [{:evl_daemon, "~> 0.3.0"}]
     end
     ```
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EvlDaemon.Mixfile do
   def project do
     [
       app: :evl_daemon,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
