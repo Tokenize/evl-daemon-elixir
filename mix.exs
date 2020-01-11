1a3898cbfc0ffe65df602a113cae7b904205e7da
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2020-01-10 23:25:58 -0500

    bump EvlDaemon version to 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EvlDaemon.Mixfile do
   def project do
     [
       app: :evl_daemon,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
