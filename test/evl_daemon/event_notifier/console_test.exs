aaefdb317f7cd32dda464c9b9774a742f99f5478
Zaid Al-Jarrah <zaid.aljarrah@me.com>
2023-11-26 21:49:47 -0500

    Fix/hack test by waiting 100 msec & logger.flush()

--- test/evl_daemon/event_notifier/console_test.exs
+++ test/evl_daemon/event_notifier/console_test.exs
@@ -4,7 +4,7 @@ defmodule EvlDaemon.EventNotifier.ConsoleTest do
   doctest EvlDaemon.EventNotifier.Console
 
   setup do
-    EvlDaemon.EventNotifier.Console.start_link()
+    start_supervised(EvlDaemon.EventNotifier.Console)
 
     :ok
   end
@@ -13,6 +13,8 @@ defmodule EvlDaemon.EventNotifier.ConsoleTest do
     output =
       capture_log(fn ->
         EvlDaemon.EventDispatcher.handle_cast({:enqueue, "5051CB", "1620960869"}, nil)
+        Process.sleep(100)
+        Logger.flush()
       end)
 
     assert Regex.match?(~r/Elixir\.EvlDaemon\.EventNotifier\.Console: \[\d+\] 505:1/, output)
